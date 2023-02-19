a2997e7cc11dcaaf80ee45c2a412488723478a38
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-02-19 09:29:06 -0800

    fix connect button disabled

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,6 +12,7 @@ export const Button = ({
   className,
   onClick,
   children,
+  disabled,
   ...buttonProps
 }: Props) => {
   const [mounted, setMounted] = useState(false);
@@ -44,6 +45,7 @@ export const Button = ({
         "inline-flex w-full items-center justify-center rounded-button border border-transparent bg-ruby-900 px-6.5 py-3 text-sm font-semibold text-white shadow-sm ring-offset-ruby-800 hover:bg-ruby-1000 focus:outline-none focus:ring-2 focus:ring-ruby-500 focus:ring-offset-2 disabled:opacity-50 sm:text-lg",
         className
       )}
+      disabled={disabled && !isConnectButton}
       onClick={handleClick}
       {...buttonProps}
     >
